e4425171d8ae4a6b8615b7ffd21922ced5041848
Priyanka-0806 <73281086+Priyanka-0806@users.noreply.github.com>
2020-12-07 10:55:05 +0530

    policy service class added

--- lifeinsurance/src/main/java/com/cognizant/service/PolicyService.java
+++ lifeinsurance/src/main/java/com/cognizant/service/PolicyService.java
@@ -1,5 +1,59 @@
 package com.cognizant.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.model.Policy;
+import com.cognizant.repository.PolicyRepo;
+
+/**
+ * 
+ * @author Priyanka Nath
+ *
+ */
+@Service
 public class PolicyService {
+
+	@Autowired
+	PolicyRepo policyRepo;
+
+	public Policy findPolicyNo(int policyNumber) {
+		return policyRepo.getOne(policyNumber);
+	}
+
+	public List<Policy> getAll() {
+		return policyRepo.findAll();
+
+	}
+
+	public void savePolicy(Policy policy) {
+		policyRepo.save(policy);
+	}
+
+	public boolean policyNumberValiadtion(Policy policy) {
+		boolean isValid = false;
+		Policy temp = findPolicyNo(policy.getPolicyNumber());
+		if (temp == null)
+			return false;
+
+		if (temp.getPolicyNumber() == policy.getPolicyNumber())
+			isValid = true;
+
+		return isValid;
+
+	}
+
+	public void getPolicyDetails(Policy policy) {
+		policy.getPolicyNumber();
+		policy.getPolicyName();
+		policy.getPolicyDescription();
+		policy.getInterestRate();
+		policy.getNetAmountPerYear();
+		policy.getPolicyTerm();
+		policy.getTotDeductible();
+		policy.getTotCoInsurance();
+	}
 
 }
