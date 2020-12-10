69e4630bd3fb903094f23b36bb99e53527c574cd
merin <merinm1998@gmail.com>
2020-12-10 17:38:18 +0530

    testing completed

--- lifeinsurance/src/test/java/com/cognizant/app/TestClaimController.java
+++ lifeinsurance/src/test/java/com/cognizant/app/TestClaimController.java
@@ -2,8 +2,10 @@ package com.cognizant.app;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +16,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.cognizant.controller.ClaimController;
@@ -50,19 +53,19 @@ public class TestClaimController {
 		this.mockMvc.perform(get("/getClaimPage")).andExpect(status().isOk());
 	}
 	
-	@Test
-	public void testClaimEditPage() throws Exception{
-		this.mockMvc.perform(get("/getClaimEdit")).andExpect(status().isOk());
-	}
 	
+//	@Test
+//	public void testreSubmitClaimPage() throws Exception{
+//		this.mockMvc.perform(MockMvcRequestBuilders.post("/getClaimEdit")).andExpect(status().is2xxSuccessful());
+//	}
+
 	@Test
-	public void postClaimEditPage() throws Exception{
-		this.mockMvc.perform(get("/getClaimEdit")).andExpect(status().isOk());
-	}
-	
-	@Test
-	public void testGetEditClaimPage() throws Exception{
-		this.mockMvc.perform(get("/getClaimEdit").param("claimNumber", "1236")).andExpect(redirectedUrl("/getClaimEdit)"));
+	public void testGetEditClaimPage() throws Exception {
+		
+		this.mockMvc.perform(get("/getClaimEdit")   
+				.param("claimNumber", "1236"))
+		.andExpect(view().name("claimedit"));
+		
 	}
 	
 }
