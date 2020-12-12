bb92cba0c276e842d38b483090b9749575848d69
Manjunath159 <manjunath.u1998@gmail.com>
2020-12-12 10:04:03 +0530

    questionnairerepo

--- lifeinsurance/src/main/java/com/cognizant/service/QuestionnaireService.java
+++ lifeinsurance/src/main/java/com/cognizant/service/QuestionnaireService.java
@@ -1,5 +1,28 @@
 package com.cognizant.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.model.Questionnaire;
+import com.cognizant.model.User;
+import com.cognizant.repository.QuestionnaireRepo;
+import com.cognizant.repository.UserRepo;
+
+@Service
 public class QuestionnaireService {
+
+	@Autowired
+	private UserRepo userRepo;
+
+	@Autowired
+	private QuestionnaireRepo questionnaireRepo;
+
+	public User findUser(int userId) {
+		return userRepo.getOne(userId);
+	}
+
+	public void saveResponse(Questionnaire questionnaire) {
+		questionnaireRepo.save(questionnaire);
+	}
 
 }
